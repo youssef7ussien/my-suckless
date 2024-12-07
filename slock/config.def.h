6c613cfcaa1b755cf046bf8dba39a57b52ecf920
youssef7ussien <youssef7ussien@gmail.com>
2024-12-07 15:38:05 +0200

    feat(slock): change colors

--- slock/config.def.h
+++ slock/config.def.h
@@ -4,8 +4,8 @@ static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INPUT] =  "#141617",   /* during input */
+	[FAILED] = "#1d2021",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
