ca5000a9057d7bfa2595e2c5b97886898598828a
youssef7ussien <youssef7ussien@gmail.com>
2025-02-01 17:53:35 +0200

    feat(dwmblocks): add microphone script

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/               /*Update Interval*/   /*Update Signal*/
+    {"",     "microphone_block",       0,                    8},
     {"",     "volume_block",           0,                    6},
     {"",     "battery_block",          30,                   5},
-    {"",     "temp_cpu_block",         6,                    7},
+    {"",     "temp_cpu_block",         5,                    7},
     {"",     "cpu_usage_block",        2,                    4},
     {"",     "ram_memory_block",       5,                    3},
     {"",     "keyboard_block",         0,                    2},
