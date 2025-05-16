84b4ab174a4e928ed5fcebd214ca7aa3f0d078e1
youssef7ussien <youssef7ussien@gmail.com>
2025-05-17 01:37:36 +0300

    feat(dwmblocks): update config.h file

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/               /*Update Interval*/   /*Update Signal*/
-    {"",     "microphone_block",       0,                    8},
-    {"",     "volume_block",           0,                    6},
-    {"",     "battery_block",          30,                   5},
-    {"",     "temp_cpu_block",         5,                    7},
-    {"",     "cpu_usage_block",        2,                    4},
-    {"",     "ram_memory_block",       5,                    3},
-    {"",     "keyboard_block",         0,                    2},
-    {"",     "clock_block",            5,                    1},
+    {"",     "microphone-block.sh",    0,                    8},
+    {"",     "volume-block.sh",        0,                    6},
+    {"",     "battery-block.sh",       30,                   5},
+    {"",     "temp-cpu-block.sh",      5,                    7},
+    {"",     "cpu-usage-block.sh",     2,                    4},
+    {"",     "ram-memory-block.sh",    5,                    3},
+    {"",     "keyboard-block.sh",      0,                    2},
+    {"",     "clock-block.sh",         5,                    1},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
